94a6a5b43dc535fc46083afb145227f69f7d0d93
ckolenchenko <ckolenchenko@yukon.cv.ua>
2021-04-02 17:37:34 +0300

    Base retrofit implementation

---
+++ lib/api_client.dart
@@ -0,0 +1,65 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+part 'api_client.g.dart';
+
+@RestApi(baseUrl: "https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/")
+abstract class RestClient {
+  factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
+
+  @GET("/tasks")
+  Future<List<Task>> getTasks();
+}
+
+@JsonSerializable()
+class Task {
+  String id;
+  String name;
+  String avatar;
+  String createdAt;
+
+  Task({this.id, this.name, this.avatar, this.createdAt});
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'],
+      name: json['name'],
+      avatar: json['avatar'],
+      createdAt: json['createdAt'],
+    );
+  }
+  // Map<String, dynamic> toJson() => _$TaskToJson(this);
+
+  // factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+  // Map<String, dynamic> toJson() => _$TaskToJson(this);
+}
+
+@RestApi(baseUrl: "https://192.168.30.63/admin/auth/")
+abstract class RestUserClient {
+  factory RestUserClient(Dio dio, {String baseUrl}) = _RestUserClient;
+
+  @GET("/userdata")
+  Future<User> getUser();
+}
+
+@JsonSerializable()
+class User {
+  int id;
+  String username;
+  String firstname;
+  String lastname;
+
+  User({this.id, this.username, this.firstname, this.lastname});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    // return null;
+
+    return User(
+      id: json['id'],
+      username: json['username'],
+      firstname: json['firstname'],
+      lastname: json['lastname'],
+    );
+  }
+}
---
+++ lib/api_client.g.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_client.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _RestClient implements RestClient {
+  _RestClient(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+    baseUrl ??= 'https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/';
+  }
+
+  final Dio _dio;
+
+  String baseUrl;
+
+  @override
+  Future<List<Task>> getTasks() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.request<List<dynamic>>('/tasks',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    var value = _result.data
+        .map((dynamic i) => Task.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+}
+
+class _RestUserClient implements RestUserClient {
+  _RestUserClient(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+    baseUrl ??= 'https://192.168.30.63/admin/auth/';
+  }
+
+  final Dio _dio;
+
+  String baseUrl;
+
+  @override
+  Future<User> getUser() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.request<Map<String, dynamic>>('/userdata',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = User.fromJson(_result.data);
+    return value;
+  }
+}
