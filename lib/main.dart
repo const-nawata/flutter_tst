eac5d10500b1d240dcba7f2b18a942f2dcbec47f
ckolenchenko <ckolenchenko@yukon.cv.ua>
2021-04-05 17:43:20 +0300

    Get user data.

--- lib/main.dart
+++ lib/main.dart
@@ -42,11 +42,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
+  User _userdata;
 
-  void _incrementCounter() {
-    setState(() {
-      // _counter++;
+  void _setUser(int userId) {
+    final _dio = Dio(); // Provide a dio instance
+    final _uclient = RestUserClient(_dio);
+    _uclient.getUser(userId).then((userdata) {
+      setState(() {
+        _userdata = userdata;
+      });
+      return userdata;
     });
   }
 
@@ -54,9 +59,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     final _dio = Dio(); // Provide a dio instance
     final _uclient = RestUserClient(_dio);
-
-    // Future<User> _user = _uclient.getUser(17).then((userdata) => userdata);
-
     Future<List<User>> _users =
         _uclient.getUsers().then((usersdata) => usersdata);
 
@@ -64,48 +66,56 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder<List<User>>(
-          future: _users,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<User> _entries = snapshot.data;
-              List<Color> _colors = [Color(0xFFE6D8FF), Color(0xFFFFD3F9)];
+      body: Column(
+        children: [
+          Text('id: ${_userdata == null ? 0 : _userdata.id}'),
+          FutureBuilder<List<User>>(
+              future: _users,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<User> _entries = snapshot.data;
+                  List<Color> _colors = [Color(0xFFE6D8FF), Color(0xFFFFD3F9)];
 
-              return Container(
-                color: Color(0xFFFFF6C1),
-                margin: EdgeInsets.only(
-                  top: 20,
-                  left: 5,
-                  right: 5,
-                ),
-                padding: EdgeInsets.only(top: 10, bottom: 10),
-                height: 400,
-                child: ListView.builder(
-                  padding: const EdgeInsets.all(5.0),
-                  itemCount: _entries.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    int _colorInd = index % 2;
-
-                    return Container(
-                      height: 35,
-                      child: Center(
-                        child: Text(
-                          'User: ${_entries[index].firstname} ${_entries[index].lastname} (${_entries[index].username})',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            fontWeight: FontWeight.bold,
-                            backgroundColor: _colors[_colorInd],
+                  return Container(
+                    color: Color(0xFFFFF6C1),
+                    margin: EdgeInsets.only(
+                      top: 20,
+                      left: 5,
+                      right: 5,
+                    ),
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    height: 400,
+                    child: ListView.builder(
+                      padding: const EdgeInsets.all(5.0),
+                      itemCount: _entries.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            _setUser(_entries[index].id);
+                          },
+                          child: Container(
+                            height: 35,
+                            child: Center(
+                              child: Text(
+                                'User: ${_entries[index].firstname} ${_entries[index].lastname} (${_entries[index].username})',
+                                style: TextStyle(
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.bold,
+                                  backgroundColor: _colors[index % 2],
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              );
-            }
+                        );
+                      },
+                    ),
+                  );
+                }
 
-            return Text('Waiting...');
-          }),
+                return Text('Waiting...');
+              }),
+        ],
+      ),
     );
   }
 }
